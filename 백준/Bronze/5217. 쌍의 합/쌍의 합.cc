32b4b40152c47138ea279aaad438c534855f57fc
wlsgh147258 <123251597+wlsgh147258@users.noreply.github.com>
2024-02-13 22:45:13 +0900

    [Bronze III] Title: 쌍의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5217. 쌍의 합/쌍의 합.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#define ll long long
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        int cnt = 0;
+        cin >> n;
+        cout << "Pairs for " << n << ": ";
+        for (int i = 1; i <= 12; i++)
+            for (int j = 1; j <= 12; j++)
+                if (i + j == n && i<j)
+                {
+                    if (!cnt)
+                    {
+                        cnt++;
+                        cout << i << ' ' << j;
+                    }
+                    else
+                        cout << ", " << i << ' ' << j;
+                }
+        cout << '\n';
+    }
+}
